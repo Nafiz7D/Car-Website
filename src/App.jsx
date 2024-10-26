d716acdd719d6874890e585624245cd4de0c133c
Nafiz7D <mrnafsu07@gmail.com>
2024-10-26 20:09:12 +0600

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
   // Dark Mode Feature
@@ -7,21 +10,34 @@ const App = () => {
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
 
-const element = document.documentElement;
-useEffect(() =>{
-  if(theme=== "dark"){
-    element.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-  } else{
-    element.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-  }
-}, [theme])
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  // AOS Initialization
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
-    <div className="max-w-7xl mx-auto">
+    <div>
       <Navbar theme={theme} setTheme={setTheme}>   
       </Navbar>
+      <Hero theme={theme}>
+      </Hero>
     </div>
   );
 };
